74ccb3eaa1a9a1ef9bbf6512853b81f67cb6607f
hIwaasa <b183r035@s.kochi-u.ac.jp>
2021-05-17 16:56:32 +0900

    .gitignoreの内容を反映

--- VIVADO/Cmod-A7_OOB/Cmod-A7_OOB.runs/user_35t_clk_wiz_0_0_synth_1/.Xil/user_35t_clk_wiz_0_0_propImpl.xdc
+++
@@ -1,3 +0,0 @@
-set_property SRC_FILE_INFO {cfile:d:/Github/releases/in-work/Cmod-A7/not-reset/Cmod-A7-35T-OOB/proj/Cmod-A7-35T-OOB.srcs/sources_1/bd/user_35t/ip/user_35t_clk_wiz_0_0/user_35t_clk_wiz_0_0.xdc rfile:../../../Cmod-A7-35T-OOB.srcs/sources_1/bd/user_35t/ip/user_35t_clk_wiz_0_0/user_35t_clk_wiz_0_0.xdc id:1 order:EARLY scoped_inst:inst} [current_design]
-set_property src_info {type:SCOPED_XDC file:1 line:57 export:INPUT save:INPUT read:READ} [current_design]
-set_input_jitter [get_clocks -of_objects [get_ports clk_in1]] 0.83333
--- VIVADO/Display3D_20201117/Display3D/Display3D.sdk/.sdk/launch_scripts/xilinx_c-c++_application_(system_debugger)/system_debugger_on_local.tcl
+++
@@ -1,9 +0,0 @@
-connect -url tcp:127.0.0.1:3121
-targets -set -filter {jtag_cable_name =~ "Digilent Cmod A7 - 35T 210328A6D9E7A" && level==0} -index 0
-fpga -file C:/proj_sasaoka/Display3D/Display3D.sdk/FPGA_TOP_hw_platform_0/FPGA_TOP.bit
-configparams mdm-detect-bscan-mask 2
-targets -set -nocase -filter {name =~ "microblaze*#0" && bscan=="USER2"  && jtag_cable_name =~ "Digilent Cmod A7 - 35T 210328A6D9E7A"} -index 0
-rst -processor
-targets -set -nocase -filter {name =~ "microblaze*#0" && bscan=="USER2"  && jtag_cable_name =~ "Digilent Cmod A7 - 35T 210328A6D9E7A"} -index 0
-dow C:/proj_sasaoka/Display3D/Display3D.sdk/DISP3D/Debug/DISP3D.elf
-bpadd -addr &main
